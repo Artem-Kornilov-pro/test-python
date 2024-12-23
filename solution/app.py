59e79cd56e9bc87820f0d1107ea5305bbbc6a768
Artem-Kornilov-pro <artemijkornilov2@gmail.com>
2024-12-23 21:39:30 +0300

    Update app.py
    Добавил выход для обработки стран , надо доработать

--- solution/app.py
+++ solution/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
-import math
+
 app = Flask(__name__)
 
 @app.route('/api/ping', methods=['GET'])
 def send():
     return jsonify({"status": "ok"}), 200
 
+@app.route('/api/countries', methods=['GET'])
+def send_countries():
+    return jsonify({"status": "ok"}), 200
+
+    """Получение списка стран с возможной фильтрацией.
+        Используется на странице регистрации для предоставления возможности выбора страны, к которой относится пользователь.
+        Если хотя бы один переданный регион является некорректным, весь запрос считается некорректным.
+        Если никакие из фильтров не переданы, необходимо вернуть все страны."""
+    
 if __name__ == "__main__":
     app.run()
+    
